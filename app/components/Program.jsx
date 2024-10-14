350907f39fa8bade7770cca6c7ad29d1aeb8c906
Samuel Garcia <samvarcia@gmail.com>
2024-10-14 18:47:56 -0500

    war checkpoint

--- app/components/Program.jsx
+++ app/components/Program.jsx
@@ -1,45 +1,41 @@
-import React from 'react';
-import Image from 'next/image';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import styles from './Program.module.css';
 
+
 const Program = () => {
+  const frames = [
+    'spiral0.png', 'spiral-0.1.png', 'spiral-0.15.png', 'spiral-0.2.png', 'spiral-0.25.png',
+    'spiral-0.3.png', 'spiral-0.35.png', 'spiral-0.4.png', 'spiral-0.45.png', 'spiral-0.5.png',
+    'spiral-0.55.png', 'spiral-0.6.png', 'spiral-0.65.png', 'spiral-0.7.png', 'spiral-0.75.png',
+    'spiral-0.8.png', 'spiral-0.85.png', 'spiral-0.9.png', 'spiral-1.0.png'
+  ];
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end end"]
+  });
+
+  const background = useTransform(
+    scrollYProgress,
+    [0, 50, 100],
+    ['url(/spiral0.png)', 'url(/spiral-0.5.png)', 'url(/spiral-0.5.png)']
+  );
+  // const backgroundImage = useTransform(
+  //   scrollYProgress,
+  //   [-100, 0, 100, 200],
+  //   [0, 1, 1, 0]
+  // )
+
   return (
-    <section className={styles.programSection}>
-      <h2 className={styles.title}>The program</h2>
-      <p className={styles.desc}>
-        Nautilus is designed as a 6-months program to give you a bit of your childhood
-        wander back. Take a break from the noise of your life to follow your curiosity and
-        get intense with your craft.
-      </p>
-      <div className={styles.contentWrapper}>
-        <div className={styles.spiralWrapper}>
-          <Image src="/full.png" alt="Program spiral" width={300} height={300} className={styles.spiralImage} />
-        </div>
-        <div className={styles.textContent}>
-          <div className={styles.column1}>
-            <h3 className={styles.subtitle}>First 3 months:</h3>
-            <p className={styles.description}>Life at the Casa Nautilus</p>
-            <ul className={styles.list}>
-              <li>Kickoff</li>
-              <li>Find yourself a rhythm</li>
-              <li>Immerse yourself into your craft</li>
-              <li>Teach your peers and be taught</li>
-              <li>Celebration banquet</li>
-            </ul>
-          </div>
-          <div className={styles.column3}>
-            <h3 className={styles.subtitle}>Last 3 months:</h3>
-            <p className={styles.description}>Your own quest</p>
-            <ul className={styles.list}>
-              <li>Dive into a new adventure</li>
-              <li>Weekly online check-ins</li>
-              <li>Deliver the documentation of your experience</li>
-              <li>Closing ceremony</li>
-            </ul>
-          </div>
-        </div>
+    <div ref={ref} className={styles.container}>
+      <div className={styles.stickyContainer}>
+        <motion.div
+          className={styles.image}
+          style={{ background }}
+        />
       </div>
-    </section>
+    </div>
   );
 };
 
