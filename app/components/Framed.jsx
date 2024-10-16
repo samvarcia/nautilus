3108953fbb27068713256d26bbcdd2b61a476c43
Samuel Garcia <samvarcia@gmail.com>
2024-10-15 19:05:47 -0500

    better quality frame3

--- app/components/Framed.jsx
+++ app/components/Framed.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './Framed.module.css';
+import Link from 'next/link';
 
 const Framed = () => {
   return (
-    <div className={styles.container}>
-      <Image 
-        src="/program1.png"
-        alt="Decorative frame"
-        layout="fill"
-        objectFit="contain"
-        className={styles.frameImage}
-      />
+    <div className={styles.frameContainer}>
+    <div className={styles.frame}>
       <div className={styles.content}>
-        <p className={styles.text}>
+      <p className={styles.text}>
           Without the pressure of <span className={styles.feature}>paying bills</span> or <span className={styles.feature}>getting grades</span>, how far can you get into your <span className={styles.feature}>craft</span>?
         </p>
+
         <button className={styles.applyButton}>
-          Apply
+          <Link href="/program" 
+          >
+            Apply
+          </Link>
         </button>
       </div>
     </div>
+  </div>
   );
 };
 
