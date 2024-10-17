64e6f9dfc6ce5d9daf45b410904d4835c34a5747
Samuel Garcia <samvarcia@gmail.com>
2024-10-16 23:00:29 -0500

    explanation update

--- app/components/Explanation.jsx
+++ app/components/Explanation.jsx
@@ -5,23 +5,27 @@ const Explanation = () => {
   return (
     <section className={styles.explanationSection}>
       <div className={styles.content}>
-        <h2 className={styles.title}>Nautilus is a six months patronage program.</h2>
+        <h2 className={styles.title}>Get intense with your craft.</h2>
         <p className={styles.description}>
-          We select a small group of talented individuals from art to science and entrepreneurship.
-          We provide you a monthly stipend, guidance, and a community of wonderful people.
-          The first 3 months, you stay in our house in San Francisco with the other participants.
-          The following 3, you go on your own quest.
+        Nautilus is a six-month program for young and quirky individuals. <br />
+        <br />
+        We select a small group of bright creatives from art, to science and entrepreneurship, providing them with a monthly stipend, personalized guidance, and a vibrant community of peers. <br />
+        <br />
+        For the first three months, you live in our San Francisco residence with fellow participants. Then, you venture out on your own, either traveling or going back home for the remaining three months. <br />
+        <br />
+        Through it all, we support you to make the most out of this experience. <br />
         </p>
-        <p className={styles.supportText}>
-          Through it all, we support you to make the most out of this experience.
-        </p>
+
         <div className={styles.cohortInfo}>
-          <span className={styles.cohortText}>NEXT COHORT STARTS ON MARCH 1ST.               <Link 
+          <span className={styles.cohortText}>
+              <Link 
                   href="https://airtable.com/appFK7duZA7g8BPxH/pagd34iOWzjCxeLzB/form" 
                   target='_blank'
                 >
-                APPLY. 
-              </Link></span>
+                APPLY TODAY. 
+              </Link>
+              {'   '} NEXT COHORT ON MARCH 1ST.               
+            </span>
         </div>
       </div> 
     </section>
