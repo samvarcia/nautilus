9402fc810820af5ebc852827b7834ca0a1927fec
Samuel Garcia <samvarcia@gmail.com>
2024-10-22 20:49:30 -0500

    framed button a crrect

--- app/components/Framed.jsx
+++ app/components/Framed.jsx
@@ -13,7 +13,7 @@ const Framed = () => {
         </p>
 
         <button className={styles.applyButton}>
-          <a href="/https://airtable.com/appFK7duZA7g8BPxH/pagd34iOWzjCxeLzB/form" 
+          <a href="https://airtable.com/appFK7duZA7g8BPxH/pagd34iOWzjCxeLzB/form" 
           target='_blank' >
             Apply
           </a>
