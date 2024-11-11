e55f5417d958852f5d990702004be61c1b3c56b1
Samuel Garcia <samvarcia@gmail.com>
2024-11-11 08:00:24 -0500

    testing form

--- app/components/SubscribeForm.jsx
+++ app/components/SubscribeForm.jsx
@@ -61,7 +61,7 @@ const SubscribeForm = () => {
         className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 disabled:opacity-50"
       >
         {isLoading ? 'Subscribing...' : 'Subscribe'}
-      </button>
+      </button>  
       </div>
       {message && (
         <p className={`mt-2 ${isError ? 'text-red-500' : 'text-green-500'}`}>
